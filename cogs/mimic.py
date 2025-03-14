04df94789d04525134ee5b30e5a28bf3ac66c941
ThomasWedding <thomaslwedding@gmail.com>
2025-03-14 19:21:54 -0400

    Add "mimic" command

---
+++ cogs/mimic.py
@@ -0,0 +1,17 @@
+# Standard inputs
+import discord
+from discord.ext import commands
+
+# Define and initialize "mimic" command
+class mimic( commands.Cog ):
+    def __init__( self, bot ):
+        self.bot = bot
+
+    # Command that simply repeats the provided input back to the user
+    @commands.command( name = "mimic", description = "Repeats provided input back to the user" )
+    async def mimic( self, ctx, *, message ):
+        await ctx.send( message )
+
+# Set up command when the bot is started
+async def setup( bot ):
+    await bot.add_cog( mimic( bot ) )
